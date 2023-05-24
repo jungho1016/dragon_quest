ef5444f5b3a93fd683ef5aba89f1c6e892747e69
Jungho <jungho1016@gmail.com>
2023-05-24 12:25:36 +0900

    1차 수정본

--- bin/cleric.dart
+++ bin/cleric.dart
@@ -3,17 +3,18 @@ import 'dart:math';
 // 연습 문제 8-1
 class Cleric {
   // 연습문제 8-2
+  static const hpMax = 50;
+  static const mpMax = 10;
   String name;
-  int hp = 50;
-  int mp = 10;
-  static final hpMax = 50;
-  static final mpMax = 10;
+  int hp;
+  int mp;
+
   // 연습 문제 9-1
 
   Cleric({
     required this.name,
-    required this.hp,
-    required this.mp,
+    this.mp = mpMax,
+    this.hp = hpMax,
   });
   // 연습문제 8-3
   void selfAid() {
@@ -27,22 +28,29 @@ class Cleric {
   }
 
   // 연습문제 8-4
-  void pray(int seconds) {
-    int? recovery = seconds + Random().nextInt(3);
-    int plusMp = recovery + mp;
-    if (seconds <= 0) {
-      print('mp 회복 되지 않습니다');
-    } else if (plusMp < mpMax) {
-      print('mp 회복 되지 않습니다');
-    } else {
-      print('mp: $plusMp');
-    }
+  int pray(int seconds) {
+    int recovery = seconds + Random().nextInt(3);
+    int regain = min(mpMax, recovery + mp);
+    mp += regain;
+    return regain;
   }
 }
 
 void main() {
+  Cleric cleric = Cleric(name: 'Cleric');
+  print('hp: ${cleric.hp}');
+  print('mp: ${cleric.mp}');
+  cleric.selfAid();
+  print('hp: ${cleric.hp}');
+  print('mp: ${cleric.mp}');
+  cleric.pray(0);
+  print(cleric.pray(3));
   // 연습 문제 9-2
   final clericA = Cleric(name: '아서스', hp: 40, mp: 5);
   final clericB = Cleric(name: '아서스', hp: 35, mp: Cleric.mpMax);
   final clericC = Cleric(name: '아서스', hp: Cleric.hpMax, mp: Cleric.mpMax);
+
+  print('name : ${clericA.name}');
+  print('hp : ${clericB.hp}');
+  print('mp : ${clericC.mp}');
 }
