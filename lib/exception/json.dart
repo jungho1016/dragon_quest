323755a6d3f6e5639eb022b44bf0265c00b8df20
Jungho <jungho1016@gmail.com>
2023-06-04 07:09:04 +0900

    1차 제출

--- lib/exception/json.dart
+++ lib/exception/json.dart
@@ -31,8 +31,12 @@ void main() {
   String jsonString =
       '{"name" : "홍길동", "age" : 41, "grop" : "총무부", "position" : "leader"}';
   Map<String, dynamic> json = jsonDecode(jsonString); // 역직렬화
-  Employee employee = Employee.fromJson(json);
-  Department department = Department.fromJson(json);
+  Employee employee = Employee.fromJson(json); // 역직렬화
+  Department department = Department.fromJson(json); // 역직렬화
+  Map<String, dynamic> emplyee1 = employee.toJson(); // 직렬화
+  Map<String, dynamic> department1 = department.toJson(); // 직렬화
+  String employee2 = jsonEncode(employee); // 직렬화
+  String department2 = jsonEncode(department); // 직렬화
 
   File file = File('company.txt');
   file.writeAsStringSync(jsonString);
